9fafb2800d9a39b307c3d840db745792dfd1b53c
Letitia Xu <letitia1995xx@gmail.com>
2015-11-22 19:03:09 +0800

    Initial Commit

---
+++ opencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by 徐欣 on 15/11/22.
+//  Copyright © 2015年 xx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
